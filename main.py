7d26508bedc16c7d49daba4574fbd41f89a6b9a1
lumahloi <magusoflowers0803@gmail.com>
2025-05-24 00:11:31 -0300

    orientations

--- main.py
+++ main.py
@@ -9,80 +9,98 @@ def read_root():
 
 ################################### AUTH
 
+# Autenticação de usuário.
 @app.post("/auth/login") # POST
 def auth_login():
     ...
 
+# Registro de novo usuário.
 @app.post("/auth/register") # POST
 def auth_register():
     ...
-    
+
+#  Refresh de token JWT.    
 @app.post("/auth/refresh-token") # POST
 def auth_refresh_token():
     ...
     
 ################################### CLIENTS
-    
+
+# Listar todos os clientes, com suporte a paginação e filtro por nome e email.    
 @app.get("/clients") # GET
 def clients_get():
     ...
-    
+
+# Criar um novo cliente, validando email e CPF únicos.    
 @app.post("/clients") # POST
 def clients_post():
     ...
-    
+
+# Obter informações de um cliente específico.    
 @app.get("/clients/{id}") # GET
 def clients_get():
     ...
 
+# Atualizar informações de um cliente específico.
 @app.put("/clients/{id}") # PUT
 def clients_put():
     ...
-    
+
+# xcluir um cliente.    
 @app.delete("/clients/{id}") # DELETE
 def clients_delete():
     ...
     
 ################################### PRODUCTS
 
+# Listar todos os produtos, com suporte a paginação e filtros por categoria, preço e disponibilidade.
 @app.get("/products") # GET
 def products_get():
     ...
     
+# Criar um novo produto, contendo os seguintes atributos: descrição, valor de venda, código de barras, seção, estoque inicial, e data de validade (quando aplicável) e imagens.    
 @app.post("/products") # POST
 def products_post():
     ...
-    
+
+# Obter informações de um produto específico.    
 @app.get("/products/{id}") # GET
 def products_get():
     ...
 
+#  Atualizar informações de um produto específico.
 @app.put("/products/{id}") # PUT
 def products_put():
     ...
-    
+
+# Excluir um produto.    
 @app.delete("/products/{id}") # DELETE
 def products_delete():
     ...
     
 ################################### PEDIDOS
 
+# Listar todos os pedidos, incluindo os seguintes filtros: período, seção dos produtos, id_pedido, status do pedido e cliente.
 @app.get("/orders") # GET
 def orders_get():
     ...
-    
+
+# Criar um novo pedido contendo múltiplos produtos, validando estoque disponível.    
 @app.post("/orders") # POST
 def orders_post():
     ...
-    
+
+# Obter informações de um pedido específico.    
 @app.get("/orders/{id}") # GET
 def orders_get():
     ...
 
+# Atualizar informações de um pedido específico, incluindo status do pedido.
 @app.put("/orders/{id}") # PUT
 def orders_put():
     ...
     
+# Excluir um pedido.
 @app.delete("/orders/{id}") # DELETE
 def orders_delete():
     ...
