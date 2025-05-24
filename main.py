8fc7b44724561019da350661d0ef7e57678ef147
Lumah Pereira <magusoflowers0803@gmail.com>
2025-05-24 17:55:25 -0300

    fix typo

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel, Field
 from typing import Union, Annotated
-from fastapi import FastAPI, Query, Depends
-from sqlmodel import SQLModel, create_engine, Session
+from fastapi import FastAPI, Query, Depends, HTTPException
+from sqlmodel import SQLModel, create_engine, Session, select, Field
+from pydantic import BaseModel
 
 
 app = FastAPI()
@@ -34,18 +34,18 @@ def on_startup():
 
 ################################### baseModels
 
-class User(BaseModel, SQLModel, table=True):
+class User(SQLModel, table=True):
     usr_id: int = Field(primary_key=True)
     usr_email: str = Query(max_length=20)
     usr_pass: str = Query(max_length=20)
     
-class Client(BaseModel, SQLModel, table=True):
+class Client(SQLModel, table=True):
     cli_id: int = Field(default=None, primary_key=True)
     cli_name: Union[str, None] = Query(max_length=20), Field(index=True)
     cli_email: Union[str, None] = Query(max_length=20), Field(index=True)
     cli_cpf: Union[str, None] = Query(max_length=11), Field(index=True)
     
-class Product(BaseModel, SQLModel, table=True):
+class Product(SQLModel, table=True):
     prod_id: int = Field(default=None, primary_key=True)
     prod_cat: str | None  = Field(index=True)
     prod_price: float = Field(index=True)
@@ -58,7 +58,7 @@ class Product(BaseModel, SQLModel, table=True):
     prod_dtval: str | None 
     prod_imgs: str
     
-class Order(BaseModel, SQLModel, table=True):
+class Order(SQLModel, table=True):
     order_id: int = Field(default=None, primary_key=True, index=True)
     order_period: Union[str, None] = Field(index=True)
     order_section: Union[str, None] = Field(index=True)
@@ -92,7 +92,9 @@ def auth_refresh_token():
 
 # Listar todos os clientes, com suporte a paginação e filtro por nome e email.    
 @app.get("/clients") # GET
-def clients_get(client: Annotated[Client, Query()], num_page: Union[int, 1]):
+def clients_get(client: Annotated[Client, Query()], num_page: Union[int, 1], session: SessionDep, offset: int = 0, limit: Annotated[int, Query(le=10)] = 10) -> list[Client]:
+    clients = session.exec(select(Client).offset(offset).limit(limit)).all()
+    return clients
     ...
 
 # Criar um novo cliente, validando email e CPF únicos.    
@@ -102,8 +104,11 @@ def clients_post(client: Annotated[Client, Query()]):
 
 # Obter informações de um cliente específico.    
 @app.get("/clients/{id}") # GET
-def clients_get():
-    ...
+def clients_get(id: int, session: SessionDep) -> Client:
+    client = session.get(Client, id)
+    if not client:
+        raise HTTPException(status_code=404, detail="Não foi possível encontrar este cliente")
+    return Client
 
 # Atualizar informações de um cliente específico.
 @app.put("/clients/{id}") # PUT
@@ -112,15 +117,21 @@ def clients_put():
 
 # Excluir um cliente.    
 @app.delete("/clients/{id}") # DELETE
-def clients_delete():
-    ...
+def clients_delete(id: int, session: SessionDep):
+    client = session.get(Client, id)
+    if not client:
+        raise HTTPException(status_code=404, detail="Não foi possível encontrar este cliente")
+    session.delete(client)
+    session.commit()
+    return {"ok": True}
     
 ################################### PRODUCTS
 
 # Listar todos os produtos, com suporte a paginação e filtros por categoria, preço e disponibilidade.
 @app.get("/products") # GET
-def products_get(product: Annotated[Product, Query()], num_page: Union[int, 1]):
-    ...
+def products_get(product: Annotated[Product, Query()], num_page: Union[int, 1], session: SessionDep, offset: int = 0, limit: Annotated[int, Query(le=10)] = 10) -> list[Product]:
+    products = session.exec(select(Product).offset(offset).limit(limit)).all()
+    return products
     
 # Criar um novo produto, contendo os seguintes atributos: descrição, valor de venda, código de barras, seção, estoque inicial, e data de validade (quando aplicável) e imagens.    
 @app.post("/products") # POST
@@ -129,8 +140,11 @@ def products_post(product: Annotated[Product, Query()]):
 
 # Obter informações de um produto específico.    
 @app.get("/products/{id}") # GET
-def products_get():
-    ...
+def products_get(id: int, session: SessionDep) -> Product:
+    product = session.get(Product, id)
+    if not product:
+        raise HTTPException(status_code=404, detail="Não foi possível encontrar este produto")
+    return Product
 
 #  Atualizar informações de um produto específico.
 @app.put("/products/{id}") # PUT
@@ -139,15 +153,21 @@ def products_put():
 
 # Excluir um produto.    
 @app.delete("/products/{id}") # DELETE
-def products_delete():
-    ...
+def products_delete(id: int, session: SessionDep):
+    product = session.get(Product, id)
+    if not product:
+        raise HTTPException(status_code=404, detail="Não foi possível encontrar este produto")
+    session.delete(product)
+    session.commit()
+    return {"ok": True}
     
 ################################### PEDIDOS
 
 # Listar todos os pedidos, incluindo os seguintes filtros: período, seção dos produtos, id_pedido, status do pedido e cliente.
 @app.get("/orders") # GET
-def orders_get(order: Annotated[Order, Query()]):
-    ...
+def orders_get(order: Annotated[Order, Query()], session: SessionDep, offset: int = 0, limit: Annotated[int, Query(le=10)] = 10) -> list[Order]:
+    orders = session.exec(select(Order).offset(offset).limit(limit)).all()
+    return orders
 
 # Criar um novo pedido contendo múltiplos produtos, validando estoque disponível.    
 @app.post("/orders") # POST
@@ -156,8 +176,11 @@ def orders_post(order: Annotated[Order, Query()]):
 
 # Obter informações de um pedido específico.    
 @app.get("/orders/{id}") # GET
-def orders_get():
-    ...
+def orders_get(id: int, session: SessionDep) -> Order:
+    order = session.get(Order, id)
+    if not order:
+        raise HTTPException(status_code=404, detail="Não foi possível encontrar este pedido")
+    return Order
 
 # Atualizar informações de um pedido específico, incluindo status do pedido.
 @app.put("/orders/{id}") # PUT
@@ -166,5 +189,10 @@ def orders_put():
     
 # Excluir um pedido.
 @app.delete("/orders/{id}") # DELETE
-def orders_delete():
-    ...
+def orders_delete(id: int, session: SessionDep):
+    order = session.get(Order, id)
+    if not order:
+        raise HTTPException(status_code=404, detail="Não foi possível encontrar este pedido")
+    session.delete(order)
+    session.commit()
+    return {"ok": True}
