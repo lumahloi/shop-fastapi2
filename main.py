2b4357e5ecda00681ae3460e478864767f22e9b1
lumahloi <magusoflowers0803@gmail.com>
2025-05-24 00:30:37 -0300

    base models

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
-from typing import Union
+from pydantic import BaseModel
 from fastapi import FastAPI
+from typing import Union
+
+################################### baseModels
+
+class User(BaseModel):
+    usr_email: str
+    usr_pass: str
+    
+class Client(BaseModel):
+    cli_name: Union[str, None]
+    cli_email: Union[str, None]
+    cli_cpf: Union[str, None]
+    
+class Product(BaseModel):
+    prod_cat: Union[str, None]
+    prod_price: Union[float, None]
+    prod_avail: Union[int, None]
+    prod_desc: str
+    prod_price: float
+    prod_barcode: str
+    prod_section: str
+    prod_stock: int
+    prod_val: str
+    prod_imgs: str
+    
+class Order(BaseModel):
+    order_period: Union[str, None]
+    order_section: Union[str, None]
+    order_id: Union[str, None]
+    order_status: Union[str, None]
+    order_cli: Union[str, None]
+    order_prods: list
+
 
 app = FastAPI()
 
@@ -11,12 +44,12 @@ def read_root():
 
 # Autenticação de usuário.
 @app.post("/auth/login") # POST
-def auth_login(usr_email: str, usr_pass: str):
+def auth_login(user: User):
     ...
 
 # Registro de novo usuário.
 @app.post("/auth/register") # POST
-def auth_register(usr_email: str, usr_pass: str):
+def auth_register(user: User):
     ...
 
 #  Refresh de token JWT.    
@@ -28,12 +61,12 @@ def auth_refresh_token():
 
 # Listar todos os clientes, com suporte a paginação e filtro por nome e email.    
 @app.get("/clients") # GET
-def clients_get(num_page: Union[int, None], cli_name: Union[str, None], cli_email: Union[str, None]):
+def clients_get(client: Client, num_page: Union[int, None]):
     ...
 
 # Criar um novo cliente, validando email e CPF únicos.    
 @app.post("/clients") # POST
-def clients_post(cli_email: str, cli_cpf: str):
+def clients_post(client: Client):
     ...
 
 # Obter informações de um cliente específico.    
@@ -55,12 +88,12 @@ def clients_delete():
 
 # Listar todos os produtos, com suporte a paginação e filtros por categoria, preço e disponibilidade.
 @app.get("/products") # GET
-def products_get(num_page: Union[int, None], prod_cat: Union[str, None], prod_price: Union[float, None], prod_avail: Union[int, None]):
+def products_get(product: Product, num_page: Union[int, None]):
     ...
     
 # Criar um novo produto, contendo os seguintes atributos: descrição, valor de venda, código de barras, seção, estoque inicial, e data de validade (quando aplicável) e imagens.    
 @app.post("/products") # POST
-def products_post(prod_desc: str, prod_price: float, prod_barcode: str, prod_section: str, prod_stock: int, prod_val: str, prod_imgs: str):
+def products_post(product: Product):
     ...
 
 # Obter informações de um produto específico.    
@@ -82,12 +115,12 @@ def products_delete():
 
 # Listar todos os pedidos, incluindo os seguintes filtros: período, seção dos produtos, id_pedido, status do pedido e cliente.
 @app.get("/orders") # GET
-def orders_get(order_period: str, order_section: str, order_id: str, order_status: str, order_cli: str):
+def orders_get(order: Order):
     ...
 
 # Criar um novo pedido contendo múltiplos produtos, validando estoque disponível.    
 @app.post("/orders") # POST
-def orders_post(order_prods: list):
+def orders_post(order: Order):
     ...
 
 # Obter informações de um pedido específico.    
