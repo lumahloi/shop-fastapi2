22bc3ddc68557bb4aeaea97b0032071c2afd2cdc
lumahloi <magusoflowers0803@gmail.com>
2025-05-24 00:49:09 -0300

    add filter to params

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-from pydantic import BaseModel
-from fastapi import FastAPI
-from typing import Union
+from pydantic import BaseModel, Field
+from typing import Union, Annotated
+from fastapi import FastAPI, Query
 
 ################################### baseModels
 
 class User(BaseModel):
-    usr_email: str
-    usr_pass: str
+    usr_email: str = Query(max_length=20)
+    usr_pass: str = Query(max_length=20)
     
 class Client(BaseModel):
-    cli_name: Union[str, None]
-    cli_email: Union[str, None]
-    cli_cpf: Union[str, None]
+    cli_name: Union[str, None] = Query(max_length=20)
+    cli_email: Union[str, None] = Query(max_length=20)
+    cli_cpf: Union[str, None] = Query(max_length=11)
     
 class Product(BaseModel):
     prod_cat: Union[str, None]
     prod_price: Union[float, None]
-    prod_avail: Union[int, None]
-    prod_desc: str
+    prod_avail: Union[bool, None]
+    prod_desc: str = Query(max_length=100)
     prod_price: float
     prod_barcode: str
     prod_section: str
-    prod_stock: int
+    prod_stock: int = Field(0, gt=0)
     prod_val: str
     prod_imgs: str
     
@@ -44,12 +44,12 @@ def read_root():
 
 # Autenticação de usuário.
 @app.post("/auth/login") # POST
-def auth_login(user: User):
+def auth_login(user: Annotated[User, Query()]):
     ...
 
 # Registro de novo usuário.
 @app.post("/auth/register") # POST
-def auth_register(user: User):
+def auth_register(user: Annotated[User, Query()]):
     ...
 
 #  Refresh de token JWT.    
@@ -61,12 +61,12 @@ def auth_refresh_token():
 
 # Listar todos os clientes, com suporte a paginação e filtro por nome e email.    
 @app.get("/clients") # GET
-def clients_get(client: Client, num_page: Union[int, 1]):
+def clients_get(client: Annotated[Client, Query()], num_page: Union[int, 1]):
     ...
 
 # Criar um novo cliente, validando email e CPF únicos.    
 @app.post("/clients") # POST
-def clients_post(client: Client):
+def clients_post(client: Annotated[Client, Query()]):
     ...
 
 # Obter informações de um cliente específico.    
@@ -88,12 +88,12 @@ def clients_delete():
 
 # Listar todos os produtos, com suporte a paginação e filtros por categoria, preço e disponibilidade.
 @app.get("/products") # GET
-def products_get(product: Product, num_page: Union[int, 1]):
+def products_get(product: Annotated[Product, Query()], num_page: Union[int, 1]):
     ...
     
 # Criar um novo produto, contendo os seguintes atributos: descrição, valor de venda, código de barras, seção, estoque inicial, e data de validade (quando aplicável) e imagens.    
 @app.post("/products") # POST
-def products_post(product: Product):
+def products_post(product: Annotated[Product, Query()]):
     ...
 
 # Obter informações de um produto específico.    
@@ -115,12 +115,12 @@ def products_delete():
 
 # Listar todos os pedidos, incluindo os seguintes filtros: período, seção dos produtos, id_pedido, status do pedido e cliente.
 @app.get("/orders") # GET
-def orders_get(order: Order):
+def orders_get(order: Annotated[Order, Query()]):
     ...
 
 # Criar um novo pedido contendo múltiplos produtos, validando estoque disponível.    
 @app.post("/orders") # POST
-def orders_post(order: Order):
+def orders_post(order: Annotated[Order, Query()]):
     ...
 
 # Obter informações de um pedido específico.    
