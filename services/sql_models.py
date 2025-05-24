01863fe298b14dbd4e2fc8c995bfa3831dcb752e
Lumah Pereira <magusoflowers0803@gmail.com>
2025-05-24 20:09:33 -0300

    fix sql_models.py

--- services/sql_models.py
+++ services/sql_models.py
@@ -1,12 +1,12 @@
-from fastapi import Query
 from sqlmodel import SQLModel, Field
 from datetime import datetime
 
+
 class UserBase(SQLModel):
-    usr_name: str = Query(max_length=40)
-    usr_email: str = Query(max_length=20)
-    usr_pass: str = Query(max_length=20)
-    usr_type: str = 'Funcionário'
+    usr_name: str = Field(max_length=40)
+    usr_email: str = Field(max_length=20)
+    usr_pass: str = Field(max_length=20)
+    usr_type: str = Field(default='Funcionário', max_length=20)
     
 class UserCreate(UserBase):
     pass
@@ -21,11 +21,11 @@ class User(UserBase, table=True):
 ######################################################################################
     
 class ClientBase(SQLModel):
-    cli_name: str = Query(max_length=20), Field(index=True)
-    cli_email: str = Query(max_length=20), Field(index=True)
-    cli_cpf: str = Query(max_length=11), Field(index=True)
-    cli_phone: str = Query(max_length=11)
-    cli_address: str = Query(max_length=100)
+    cli_name: str = Field(max_length=20), Field(index=True)
+    cli_email: str = Field(max_length=20), Field(index=True)
+    cli_cpf: str = Field(max_length=11), Field(index=True)
+    cli_phone: str = Field(max_length=11)
+    cli_address: str = Field(max_length=100)
     
 class ClientCreate(ClientBase):
     pass
@@ -39,8 +39,8 @@ class Client(ClientBase, table=True):
     
 class ProductBase(SQLModel):
     prod_id: int = Field(default= None, primary_key=True)
-    prod_name: str = Query(max_length=100), Field(index=True)
-    prod_desc: str = Query(max_length=100)
+    prod_name: str = Field(max_length=100), Field(index=True)
+    prod_desc: str = Field(max_length=100)
     prod_price: float = Field(index=True)
     prod_stock: int = Field(0, gt=0)
     # prod_size: list
@@ -69,7 +69,7 @@ class OrderBase(SQLModel):
     order_cli: int = Field(index=True)
     order_total: float
     order_typepay: str
-    order_address: str = Query(max_length=100)
+    order_address: str = Field(max_length=100)
     order_section: str = Field(index=True)
     # order_prods: list
     
