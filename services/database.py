cbcc6bcadb675792f9db6d86c6c4103d6883003f
Lumah Pereira <magusoflowers0803@gmail.com>
2025-05-24 20:09:02 -0300

    feat drop

--- services/database.py
+++ services/database.py
@@ -9,5 +9,8 @@ password = 1234567890
 postgresql_url = f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{postgresql_file_name}"
 engine = create_engine(postgresql_url)
 
+
 def create_db_and_tables():
+    SQLModel.metadata.drop_all(engine)
     SQLModel.metadata.create_all(engine)
+#     SQLModel.metadata.create_all(engine)
