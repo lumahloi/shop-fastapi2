5589fc8b8bfb51e2d72fcda844d0a5ce1063b429
Lumah Pereira <magusoflowers0803@gmail.com>
2025-05-25 10:04:06 -0300

    refactor types

--- services/sql_models.py
+++ services/sql_models.py
@@ -2,17 +2,9 @@ from sqlmodel import SQLModel, Field, JSON
 from sqlalchemy import Column
 from datetime import datetime
 from typing import List
-from enum import Enum
+from types import UserType, SizeType, ColorType, CategoryType, SectionType, StatusType, PaymentType
 
 
-
-class UserType(str, Enum):
-    administrador = "Administrador"
-    gerente = "Gerente"
-    vendedor = "Vendedor"
-    marketing = "Marketing"
-    estoquista = "Estoquista"
-    atendente = "Atendente"
 
 class UserBase(SQLModel):
     usr_name: str = Field(min_length=3, max_length=20, index=True)
@@ -58,43 +50,6 @@ class Client(ClientBase, table=True):
 
 ######################################################################################
 
-class SizeType(str, Enum):
-    tam_pp = "PP"
-    tam_P = "P"
-    tam_M = "M"
-    tam_G = "G"
-    tam_GG = "GG"
-    
-class ColorType(str, Enum):
-    color_amarelo = "amarelo"
-    color_azul = "azul"
-    color_bege = "bege"
-    color_branco = "branco"
-    color_bronze = "bronze"
-    color_ciano = "ciano"
-    color_cinza = "cinza"
-    color_laranja = "laranja"
-    color_lilás = "lilás"
-    color_marrom = "marrom"
-    color_preto = "preto"
-    color_rosa = "rosa"
-    color_roxo = "roxo"
-    color_verde = "verde"
-    color_vermelho = "vermelho"
-    
-class CategoryType(str, Enum):
-    masc = "Masculino"
-    fem = "Feminino"
-    k_fem = "Menina"
-    k_masc = "Menino"
-    
-class SectionType(str, Enum):
-    Blusas = "Blusas"
-    Calças = "Calças"
-    Vestidos = "Vestidos"
-    Calçados = "Blusas"
-    Shorts = "Shorts"
-    Acessórios = "Blusas"
 
 class ProductBase(SQLModel):
     prod_size: List[SizeType] = Field(default_factory=list, sa_column=Column(JSON))
@@ -132,21 +87,6 @@ class Product(ProductBase, table=True):
     prod_lastupdate: datetime = Field(default_factory=datetime.utcnow)
 
 ######################################################################################
-class StatusType(str, Enum):
-    andamento = "Em andamento"
-    pagamentook = "Pagamento confirmado"
-    preparando = "Preparando para a entrega"
-    enviado = "Enviado para a entrega"
-    entregue = "Entregue"
-    acaminho = "A caminho"
-    cancelado = "Cancelado"
-    reembolso = "Solicitado reembolso"
-    
-class PaymentType(str, Enum):
-    credito = "Crédito"
-    debito = "Débito"
-    pix = "Pix"
-    boleto = "Boleto"
     
 class OrderBase(SQLModel):
     order_cli: int = Field(index=True)
