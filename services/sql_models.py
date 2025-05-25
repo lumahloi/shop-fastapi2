be000117e82b23b783d75fe6991cb83d88a63bae
Lumah Pereira <magusoflowers0803@gmail.com>
2025-05-24 21:01:15 -0300

    fix slqmoodels

--- services/sql_models.py
+++ services/sql_models.py
@@ -1,12 +1,13 @@
 from sqlmodel import SQLModel, Field
 from datetime import datetime
+from pydantic import constr
 
 
 class UserBase(SQLModel):
-    usr_name: str = Field(max_length=40)
-    usr_email: str = Field(max_length=20)
-    usr_pass: str = Field(max_length=20)
-    usr_type: str = Field(default='Funcionário', max_length=20)
+    usr_name: str = Field(min_length=3, max_length=20, index=True)
+    usr_email: str = Field(min_length=10,max_length=25,index=True)
+    usr_pass: str = Field(min_length=8,max_length=20)
+    usr_type: str = Field(min_length=5,default='Funcionário', max_length=20)
     
 class UserCreate(UserBase):
     pass
@@ -21,11 +22,11 @@ class User(UserBase, table=True):
 ######################################################################################
     
 class ClientBase(SQLModel):
-    cli_name: str = Field(max_length=20), Field(index=True)
-    cli_email: str = Field(max_length=20), Field(index=True)
-    cli_cpf: str = Field(max_length=11), Field(index=True)
-    cli_phone: str = Field(max_length=11)
-    cli_address: str = Field(max_length=100)
+    cli_name: str = Field(min_length=3,max_length=20,index=True)
+    cli_email: str = Field(min_length=10,max_length=25,index=True)
+    cli_cpf: str = Field(min_length=11,max_length=14,index=True)
+    cli_phone: str = Field(min_length=11,max_length=15)
+    cli_address: str = Field(min_length=8,max_length=100)
     
 class ClientCreate(ClientBase):
     pass
@@ -38,21 +39,17 @@ class Client(ClientBase, table=True):
 ######################################################################################
     
 class ProductBase(SQLModel):
-    prod_id: int = Field(default= None, primary_key=True)
-    prod_name: str = Field(max_length=100), Field(index=True)
+    prod_name: str = Field(min_length=3,max_length=50,index=True)
     prod_desc: str = Field(max_length=100)
     prod_price: float = Field(index=True)
-    prod_stock: int = Field(0, gt=0)
+    prod_stock: int = Field(default=0, gt=0)
     # prod_size: list
     # prod_color: list
-    prod_cat: str = Field(index=True)
+    prod_cat: str = Field(min_length=7,index=True)
     # prod_imgs: list
-    prod_active: bool = Field(index=True)
-    prod_barcode: str 
-    prod_section: str 
-    prod_dtval: str 
-    prod_createdat: str
-    prod_lastupdate: str
+    prod_barcode: str = Field(min_length=43,max_length=43)
+    prod_section: str = Field(min_length=7,index=True)
+    prod_dtval: str = datetime
     
 class ProductCreate(ProductBase):
     pass
@@ -68,9 +65,9 @@ class Product(ProductBase, table=True):
 class OrderBase(SQLModel):
     order_cli: int = Field(index=True)
     order_total: float
-    order_typepay: str
-    order_address: str = Field(max_length=100)
-    order_section: str = Field(index=True)
+    order_typepay: str = Field(min_length=3,max_length=20),
+    order_address: str = Field(min_length=8,max_length=100)
+    order_section: str = Field(min_length=7,max_length=15,index=True)
     # order_prods: list
     
 class OrderCreate(OrderBase):
@@ -79,5 +76,5 @@ class OrderCreate(OrderBase):
 class Order(OrderBase, table=True):
     order_id: int = Field(primary_key=True, index=True)
     order_period: str = Field(index=True)
-    order_status: str = 'Em andamento'
+    order_status: str = Field(default="Em andamento",min_length=3,max_length=20)
     order_createdat: datetime = Field(default_factory=datetime.utcnow)
