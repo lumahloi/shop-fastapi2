db9ca00a3628fd6b2f8dece0924385a4d5f9d58c
lumahloi <magusoflowers0803@gmail.com>
2025-05-26 08:54:41 -0300

    drop tables

--- app/utils/database.py
+++ app/utils/database.py
@@ -1,4 +1,5 @@
 from sqlmodel import SQLModel, create_engine
+from sqlalchemy import text
 
 postgresql_file_name = "shop-fastapi"
 port = 5432
@@ -11,5 +12,8 @@ engine = create_engine(postgresql_url)
 
 
 def create_db_and_tables():
-    SQLModel.metadata.drop_all(bind=engine)
+    with engine.connect() as conn:
+        conn.execute(text("DROP SCHEMA public CASCADE;"))
+        conn.execute(text("CREATE SCHEMA public;"))
+        conn.commit()
     SQLModel.metadata.create_all(bind=engine)
